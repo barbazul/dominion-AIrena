4abaf927fb3976f0061f6ac724263853bba7c179
Matias Montes <matias@semexpert.com.ar>
2019-02-26 20:30:40 -0300

    Removing bug comment from Remodel. Created an Issue in tracker

--- src/cards/remodel.js
+++ src/cards/remodel.js
@@ -43,40 +43,6 @@ export default class Remodel extends BasicAction {
       }
     }
 
-    // Todo found a bug:
-    // TypeError: Cannot read property 'trash' of null
-    // at Remodel.playEffect (file:///C:/wamp64/www/dominion/src/cards/remodel.js:48:41)
-    // at Remodel.onPlay (file:///C:/wamp64/www/dominion/src/cards/card.js:103:10)
-    // at State.resolveAction (file:///C:/wamp64/www/dominion/src/game/state.js:220:12)
-    // at ThroneRoom.playEffect (file:///C:/wamp64/www/dominion/src/cards/throneRoom.js:37:13)
-    // at ThroneRoom.onPlay (file:///C:/wamp64/www/dominion/src/cards/card.js:103:10)
-    // at State.resolveAction (file:///C:/wamp64/www/dominion/src/game/state.js:220:12)
-    // at ThroneRoom.playEffect (file:///C:/wamp64/www/dominion/src/cards/throneRoom.js:37:13)
-    // at ThroneRoom.onPlay (file:///C:/wamp64/www/dominion/src/cards/card.js:103:10)
-    // at State.resolveAction (file:///C:/wamp64/www/dominion/src/game/state.js:220:12)
-    // at State.playAction (file:///C:/wamp64/www/dominion/src/game/state.js:208:10)
-    //
-    // Nuevo caso. Mas info:
-    // cleanup phase
-    // (SillyAI shuffles.)
-    // SillyAI draws 5 cards: Cellar,Copper,Remodel,Cellar,Duchy.
-    // [...]
-    // === SillyAI's turn 15
-    // start phase
-    // action phase
-    // SillyAI plays Cellar.
-    // SillyAI discards Duchy.
-    // SillyAI discards Copper.
-    // SillyAI draws 2 cards: Estate,Throne Room.
-    // SillyAI plays Throne Room.
-    // SillyAI plays Cellar.
-    // SillyAI discards Estate.
-    // SillyAI draws 1 cards: Throne Room.
-    // ...plays Cellar again.
-    // SillyAI draws 0 cards: .
-    // SillyAI plays Throne Room.
-    // SillyAI plays Remodel.
-    // (crash)
     choice = state.current.agent.choose(BasicAI.CHOICE_UPGRADE, state, choices);
 
     state.doTrash(state.current, choice.trash[0]);
