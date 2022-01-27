859361eb4eac29b9bac7919c1e082239caed6152
Barbazul <mmontes@lyracons.com>
2022-01-27 12:25:27 -0300

    More coverage for Single Baron

--- src/agents/dominiate/__tests__/singleBaron.test.js
+++ src/agents/dominiate/__tests__/singleBaron.test.js
@@ -1,6 +1,6 @@
-import SingleBaron from "../singleBaron.js";
-import State from "../../../game/state.js";
-import cards from "../../../game/cards.js";
+import SingleBaron from '../singleBaron.js';
+import State from '../../../game/state.js';
+import cards from '../../../game/cards.js';
 
 const muteConfig = { log: () => {} };
 
@@ -17,6 +17,51 @@ test('gainPriority at game start', () => {
   );
 });
 
+test('Duchy dancing', () => {
+  const agent = new SingleBaron();
+  const state = new State();
+
+  state.setUp([agent, agent], muteConfig);
+  state.gainsToEndGame = () => 5;
+
+  const priority = agent.gainPriority(state, state.current);
+
+  expect(priority).toEqual(
+    [ cards.Province, cards.Duchy, cards.Gold, cards.Baron, cards.Silver ]
+  );
+});
+
+test('End game', () => {
+  const agent = new SingleBaron();
+  const state = new State();
+
+  state.setUp([agent, agent], muteConfig);
+  state.gainsToEndGame = () => 2;
+
+  const priority = agent.gainPriority(state, state.current);
+
+  expect(priority).toEqual(
+    [
+      cards.Province, cards.Duchy, cards.Estate, cards.Gold, cards.Baron,
+      cards.Silver, cards.Copper
+    ]
+  );
+});
+
+test('Only buy a single Baron', () => {
+  const agent = new SingleBaron();
+  const state = new State();
+
+  state.setUp([agent, agent], muteConfig);
+  state.current.draw.push(cards.Baron);
+
+  const priority = agent.gainPriority(state, state.current);
+
+  expect(priority).toEqual(
+    [ cards.Province, cards.Gold, cards.Silver ]
+  );
+});
+
 test('discardPriority at game start', () => {
   const agent = new SingleBaron();
   const state = new State();
@@ -30,6 +75,23 @@ test('discardPriority at game start', () => {
     [
       cards.Province, cards.Duchy, cards.Curse, cards.Estate, cards.Copper,
       cards.Baron, null, cards.Silver, cards.Estate, cards.Baron
+    ]
+  );
+});
+
+test('Keep single Estate with Baron in hand', () => {
+  const agent = new SingleBaron();
+  const state = new State();
+
+  state.setUp([agent, agent], muteConfig);
+  state.current.hand = [ cards.Baron, cards.Estate ];
+
+  const priority = agent.discardPriority(state, state.current);
+
+  expect(priority).toEqual(
+    [
+      cards.Province, cards.Duchy, cards.Curse, cards.Copper, null,
+      cards.Silver, cards.Estate, cards.Baron
     ]
   );
 });
