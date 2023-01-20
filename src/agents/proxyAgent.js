e4306fa12327e3c0bca32cccdf4c46ebbaf3f403
Barbazul <terevalis@gmail.com>
2023-01-20 09:40:45 -0300

    Added an additional proxyMethod to ProxyAgento for DomPlayer compatibility

--- src/agents/proxyAgent.js
+++ src/agents/proxyAgent.js
@@ -53,4 +53,36 @@ export default class ProxyAgent extends BasicAI {
 
     return newAgent;
   }
+
+  /**
+   * Temporary proxy method for DomPlayer agents
+   *
+   * @deprecated
+   * @todo Use https://gist.github.com/loilo/4d385d64e2b8552dcc12a0f5126b6df8 to have magic methods
+   * @param {Player} my
+   * @return {int}
+   */
+  getTotalMoney (my) {
+    if (this.actualAgent.getTotalMoney) {
+      return this.actualAgent.getTotalMoney(my);
+    }
+
+    return 0;
+  }
+
+  /**
+   * Temporary proxy method for DomPlayer agents
+   *
+   * @param {Player} my
+   * @param {State} state
+   * @param {Card} cardToTrash
+   * @return {boolean}
+   */
+  removingReducesBuyingPower (my, state, cardToTrash) {
+    if (this.actualAgent.removingReducesBuyingPower) {
+      return this.actualAgent.removingReducesBuyingPower(my, state, cardToTrash);
+    }
+
+    return false;
+  }
 }
